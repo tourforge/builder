ffde06cef2b80843cd155cbdf7d0ebb362c1ceb1
invpt <57822954+invpt@users.noreply.github.com>
2023-01-01 23:44:12 -0500

    Break tour editor into its own page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,5 @@
-import { useEffect, useRef } from "react";
-import { RecoilRoot } from "recoil";
-
-import Head from "next/head";
-
-import Modal from "react-modal";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
-import { LatLng } from "../src/data";
-
-import Map from "../components/map";
-import Sidebar from "../components/sidebar";
-
-import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  const mapCenter = useRef<LatLng>({ "lat": 0, "lng": 0 });
-
-  useEffect(() => {
-    Modal.setAppElement(document.getElementById("__next")!);
-  }, []);
-
-  return (
-    <RecoilRoot>
-      <Head>
-        <title>OpenTourBuilder</title>
-        <meta name="description" content="" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <main className={styles.main}>
-        <Sidebar mapCenter={mapCenter} />
-        <Map centerRef={mapCenter} />
-      </main>
-      <ToastContainer
-        position="bottom-left"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
-    </RecoilRoot>
-  );
+  return <p>Welcome to OpenTourBuilder! <Link href="/tour-editor">Edit</Link> </p>;
 }
