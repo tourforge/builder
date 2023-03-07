56f04c5f370dc6dcafc96f35e597cc14643db34e
invpt <57822954+invpt@users.noreply.github.com>
2023-03-07 00:20:42 -0500

    Fix import in pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import Modal from "components/tour-editor/modal";
+import Modal from "components/modal";
 import Head from "next/head";
 import Link from "next/link";
 import { ChangeEvent, useEffect, useId, useState } from "react";
