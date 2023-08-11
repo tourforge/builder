bf7847aa58df73164802630eb5ac73011161b009
invpt <57822954+invpt@users.noreply.github.com>
2023-08-11 12:34:30 -0400

    Change api/auth/ to auth/

--- backend/opentourbuilder/urls.py
+++ backend/opentourbuilder/urls.py
@@ -28,5 +28,5 @@ router.register(r'users', UserViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('api/auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
