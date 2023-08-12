19fe31468aed3954e3286c98da2d9468918e0c6f
invpt <57822954+invpt@users.noreply.github.com>
2023-08-12 00:21:32 -0400

    Add view of user.username field to ProjectMemberSerializer

--- backend/opentourbuilder/serializers.py
+++ backend/opentourbuilder/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import relations
-from rest_framework.serializers import HyperlinkedModelSerializer
+from rest_framework.serializers import HyperlinkedModelSerializer, HyperlinkedRelatedField, CharField
 from rest_framework_nested.serializers import NestedHyperlinkedModelSerializer
 from rest_framework_nested.relations import NestedHyperlinkedRelatedField
 
@@ -31,7 +31,8 @@ class ProjectMemberSerializer(NestedHyperlinkedModelSerializer):
     }
 
     project = NestedHyperlinkedRelatedField(read_only=True, view_name='project-detail', parent_lookup_kwargs=parent_lookup_kwargs)
-    user = NestedHyperlinkedRelatedField(queryset=User.objects.all(), view_name='user-detail', parent_lookup_kwargs={})
+    user = HyperlinkedRelatedField(queryset=User.objects.all(), view_name='user-detail')
+    username = CharField(read_only=True, source='user.username')
 
     class Meta:
         model = ProjectMember
